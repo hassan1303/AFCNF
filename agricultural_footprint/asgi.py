86202aa530244bdb497b224e06080376eb7d970e
Hassan Muhammad Adam <hassanmuhammadadam1@gmail.com>
2025-05-16 03:00:19 -0700

    updates

---
+++ agricultural_footprint/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for agricultural_footprint project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agricultural_footprint.settings')
+
+application = get_asgi_application()
