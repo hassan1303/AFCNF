d4bf552d16a060797047528ffa9644b161017f3f
Hassan Muhammad Adam <hassanmuhammadadam1@gmail.com>
2025-05-16 09:08:10 -0700

    updates

--- farmers/admin.py
+++ farmers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(FarmDetail)
+admin.site.register(CropData)
+admin.site.register(LivestockData)
+admin.site.register(WaterUsage)
+admin.site.register(MachineryUse)
