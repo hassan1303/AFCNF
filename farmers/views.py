1f196e959573a6b41d9c62852d0e532fca5e8fc5
Hassan Muhammad Adam <hassanmuhammadadam1@gmail.com>
2025-05-16 09:45:19 -0700

    updates

--- farmers/views.py
+++ farmers/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from farmers.models import WaterUsage
+
 
 # Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def submit_data(request):
+    # Example check
+    if WaterUsage > 1000:
+        messages.warning(request, "⚠️ High water usage detected. Consider drip irrigation.")
+    messages.success(request, "✅ Data submitted successfully.")
+    return redirect('dashboard')
